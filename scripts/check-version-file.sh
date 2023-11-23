98723d5ab3293c448fdde8bbb33bf6abbfb2ba63
Max <hello@mpfglaser.nl>
2023-11-23 13:57:46 +0100

    chore(ci): update version check script
    to use Helm Chart.yaml files rather than arbitrary version file

--- scripts/check-version-file.sh
+++ scripts/check-version-file.sh
@@ -1,10 +1,10 @@
 # Get a list of affected apps
-affected_apps=$(npx nx print-affected --base=origin/main..HEAD --select=projects)
+affected_apps=$(npx nx show projects affected --with-target serve --base=origin/main..HEAD)
 
 for app in $affected_apps
 do
   # Check if the version file in the app's directory has changed
-  version_file_change=$(git diff origin/main..HEAD --quiet -- "apps/$app/version.txt" || echo "changed")
+  version_file_change=$(git diff origin/main..HEAD --quiet -- "helm/$app/Chart.yaml" || echo "changed")
 
   if [ "$version_file_change" != "changed" ]
   then
