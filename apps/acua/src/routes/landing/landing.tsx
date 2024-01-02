73891fba3758dc21808701e051661be1561217c1
Max <hello@mpfglaser.nl>
2024-01-02 13:08:42 +0100

    feat(acua): improve landing page

--- apps/acua/src/routes/landing/landing.tsx
+++ apps/acua/src/routes/landing/landing.tsx
@@ -1,13 +1,77 @@
-import styles from './landing.module.scss';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 
 /* eslint-disable-next-line */
 export interface LandingProps {}
 
 export function Landing(props: LandingProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Landing page</h1>
-    </div>
+    <Container sx={{ my: 1 }}>
+      <Box>
+        <Typography variant="h4" component="h1">
+          Welcome to the Apartment Complex Utility App!
+        </Typography>
+        <Typography variant="body1" component="p">
+          Here you can file repair requests and manage your account information.
+        </Typography>
+      </Box>
+      <Divider variant="middle" sx={{ my: 2 }} />
+      <Grid container spacing={2}>
+        <Grid item>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea component={Link} to="/tickets">
+              <CardMedia
+                component="img"
+                height="140"
+                image="/public/images/repair.jpg"
+                alt="Pliers and a hammer"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Repair Requests
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Something broken in or around your apartment? File a repair
+                  request, and we'll take care of it.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea component={Link} to="/edit-profile">
+              <CardMedia
+                component="img"
+                height="140"
+                image="/public/images/couple.jpg"
+                alt="Pliers and a hammer"
+                sx={{ objectPosition: 'top' }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Manage your account
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Has something changed? View and manage your account
+                  information.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
