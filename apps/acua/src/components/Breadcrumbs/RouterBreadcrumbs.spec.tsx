ba6449bb156fc513889afd8a1c9d0a951ba0286c
Max <hello@mpfglaser.nl>
2024-01-02 18:55:38 +0100

    refactor(acua): remove leftover file

--- apps/acua/src/components/Breadcrumbs/RouterBreadcrumbs.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import RouterBreadcrumbs from '../RouterBreadcrumbs/RouterBreadcrumbs';
-
-describe('RouterBreadcrumbs', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<RouterBreadcrumbs />);
-    expect(baseElement).toBeTruthy();
-  });
-});
