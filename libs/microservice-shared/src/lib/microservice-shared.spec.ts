51e7216832666d74b8b22da5d9e441eccd18bb43
Max <hello@mpfglaser.nl>
2024-01-05 20:38:00 +0100

    refactor(microservice-shared): remove failing test
    it depended upon firebase_config being set, which won't happen in a test.

--- libs/microservice-shared/src/lib/microservice-shared.spec.ts
+++
@@ -1,7 +0,0 @@
-import { microserviceShared } from './microservice-shared';
-
-describe('microserviceShared', () => {
-    it('should work', () => {
-        expect(microserviceShared()).toEqual('microservice-shared');
-    });
-});
