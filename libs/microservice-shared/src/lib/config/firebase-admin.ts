601fd96c1e70ef49c1691761518c24b280cf5f99
Max <hello@mpfglaser.nl>
2024-01-06 22:05:46 +0100

    fix(microservice-shared/firebase): add function to trim trailing newline, should fix error.

--- libs/microservice-shared/src/lib/config/firebase-admin.ts
+++ libs/microservice-shared/src/lib/config/firebase-admin.ts
@@ -2,11 +2,14 @@ import { ServiceAccount, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { credential } from 'firebase-admin';
 
-const serviceAccount = process.env['FIREBASE_CONFIG'];
+let serviceAccount = process.env['FIREBASE_CONFIG'];
 
 if (!serviceAccount) {
   throw new Error('FIREBASE_CONFIG not set');
 }
+
+// remove trailing newline, if any
+serviceAccount = serviceAccount.replace(/\n$/, '');
 
 initializeApp({
   credential: credential.cert(JSON.parse(serviceAccount) as ServiceAccount),
