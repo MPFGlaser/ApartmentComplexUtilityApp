1efd9b0ffc150c44e93e54e20f2c2feaf5eed15d
Max <hello@mpfglaser.nl>
2024-01-10 11:32:03 +0100

    refactor(firebase): remove need for user to set FIREBASE_CONFIG in local env

--- libs/microservice-shared/src/lib/config/firebase-admin.ts
+++ libs/microservice-shared/src/lib/config/firebase-admin.ts
@@ -1,24 +1,10 @@
-import { ServiceAccount, initializeApp } from 'firebase-admin/app';
+import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { credential } from 'firebase-admin';
 
-let serviceAccount: string | undefined;
-
-if (process.env['NODE_ENV'] === 'development') {
-  serviceAccount = process.env['FIREBASE_CONFIG'];
-
-  if (!serviceAccount) {
-    throw new Error('FIREBASE_CONFIG not set');
-  }
-
-  // remove trailing newline, if any
-  serviceAccount = serviceAccount.replace(/\n$/, '');
-}
-
 initializeApp({
-  credential: serviceAccount
-    ? credential.cert(JSON.parse(serviceAccount) as ServiceAccount)
-    : credential.applicationDefault(),
+  credential: credential.applicationDefault(),
+  projectId: 'mpfg-acua',
 });
 
 export const auth = getAuth();
