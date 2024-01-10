bb0ef9df37968a82bca2917609315b85ee5b2e0c
Max <hello@mpfglaser.nl>
2024-01-10 22:14:29 +0100

    chore(firebase): set up emulators

--- libs/microservice-shared/src/lib/config/firebase-admin.ts
+++ libs/microservice-shared/src/lib/config/firebase-admin.ts
@@ -2,6 +2,11 @@ import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { credential } from 'firebase-admin';
 
+if (process.env['NODE_ENV'] !== 'production')
+  process.env['FIREBASE_AUTH_EMULATOR_HOST'] = 'localhost:9099';
+
+console.log('Initializing Firebase Admin SDK');
+console.log(process.env['FIREBASE_AUTH_EMULATOR_HOST']);
 initializeApp({
   credential: credential.applicationDefault(),
   projectId: 'mpfg-acua',
