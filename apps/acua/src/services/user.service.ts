b0bf84d4cbc4892b4ce62d4a29cac7601b7b874c
Max <hello@mpfglaser.nl>
2024-01-10 11:01:50 +0100

    refactor(acua/user-service): support anonymous entries

--- apps/acua/src/services/user.service.ts
+++ apps/acua/src/services/user.service.ts
@@ -5,6 +5,10 @@ class UserService extends ApiService {
   private url = `${this.baseUrl}/user`;
 
   public async getDisplayNameByUid(uid: string) {
+    if (uid === 'anonymous') {
+      return 'Anonymous';
+    }
+
     const response = await axios.get(`${this.url}/${uid}/display-name`);
 
     return response.data.name;
