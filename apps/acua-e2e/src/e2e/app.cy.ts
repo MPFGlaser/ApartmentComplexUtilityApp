365634314d8700a172074beca38c2a51816136f2
Max <hello@mpfglaser.nl>
2024-01-13 17:14:59 +0100

    test(e2e): fix greeting check

--- apps/acua-e2e/src/e2e/app.cy.ts
+++ apps/acua-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('acua', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome acua');
+    getGreeting().contains('Welcome to the Apartment Complex Utility App');
   });
 });
