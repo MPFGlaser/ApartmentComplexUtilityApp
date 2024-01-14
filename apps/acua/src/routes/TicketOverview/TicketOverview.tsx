8464cbd05835f145ccf2a304a6763bd19fb18f25
Max <hello@mpfglaser.nl>
2024-01-14 20:38:09 +0100

    debug(acua): make ticket error generic again

--- apps/acua/src/routes/TicketOverview/TicketOverview.tsx
+++ apps/acua/src/routes/TicketOverview/TicketOverview.tsx
@@ -22,7 +22,7 @@ export function TicketOverview(props: TicketOverviewProps) {
       })
       .catch((error) => {
         showSnackbar({
-          message: error.message,
+          message: 'Error fetching tickets',
           severity: 'error',
         });
       });
