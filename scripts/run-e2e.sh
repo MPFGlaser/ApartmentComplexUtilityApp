bda9bd3c7aee6c4f3ea48a63b647efae8d422073
Max <hello@mpfglaser.nl>
2024-01-14 23:17:07 +0100

    chore(gha/e2e): update script

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -4,9 +4,9 @@
 npx nx run-many --target=serve --all --exclude=acua &
 
 # Wait for all microservices to start
-npx wait-on http://localhost:3100/health
-npx wait-on http://localhost:3200/health
-npx wait-on http://localhost:3300/health
+npx wait-on http://localhost:3100/health && printf "location-service is healthy\n"
+npx wait-on http://localhost:3200/health && printf "ticket-service is healthy\n"
+npx wait-on http://localhost:3300/health && printf "user-service is healthy\n"
 
 # Run e2e tests
 npx nx e2e acua-e2e
