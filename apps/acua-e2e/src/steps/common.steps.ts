0f12bef2cc5b3dbd849d7b256c4cacd8584ff43f
Max <hello@mpfglaser.nl>
2024-01-14 16:59:25 +0100

    test(e2e): add login feature

--- apps/acua-e2e/src/steps/common.steps.ts
+++ apps/acua-e2e/src/steps/common.steps.ts
@@ -1,10 +1,53 @@
-import { When } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
 import { PlaywrightWorld } from '@gnuechtel/nx-cucumber';
+import { expect } from 'playwright/test';
+import { maskColor } from '../shared';
 
 // Given + When
-When(
-  /^(?:\w+.*?) (?:has found|visits) the home page$/,
-  async function (this: PlaywrightWorld) {
-    await this.visit('/');
+When(/^.* user visits the home page$/, async function (this: PlaywrightWorld) {
+  await this.visit('/');
+});
+
+Then(
+  'the user is shown a toast with the message {string}',
+  async function (this: PlaywrightWorld, message: string) {
+    const toast = this.page.getByText(message);
+
+    this.setScreenshotOptions({ mask: [toast], maskColor }); // highlight toast
+
+    await expect(toast).toBeVisible();
+  }
+);
+
+Then(
+  'the user is redirected to the {string} page',
+  async function (this: PlaywrightWorld, page: string) {
+    if (page === 'home') {
+      const homePage = this.page.locator('h1').getByText('Home');
+      const homeUrl = this.page.url();
+
+      this.setScreenshotOptions({ mask: [homePage], maskColor }); // highlight home page
+
+      await expect(homePage).toBeVisible();
+      await expect(homeUrl).toContain('/');
+    } else if (page === 'login') {
+      const loginPage = this.page.locator('h1').getByText('Login');
+      const loginUrl = this.page.url();
+
+      this.setScreenshotOptions({ mask: [loginPage], maskColor }); // highlight login page
+
+      await expect(loginPage).toBeVisible();
+      await expect(loginUrl).toContain('/login');
+    } else if (page === 'profile') {
+      const profilePage = this.page.locator('h1').getByText('Edit Profile');
+      const homeUrl = this.page.url();
+
+      this.setScreenshotOptions({ mask: [profilePage], maskColor }); // highlight home page
+
+      await expect(profilePage).toBeVisible();
+      await expect(homeUrl).toContain('/edit-profile');
+    } else {
+      throw new Error(`Unknown page: ${page}`);
+    }
   }
 );
--- apps/acua-e2e/src/steps/homepage.steps.ts
+++ apps/acua-e2e/src/steps/homepage.steps.ts
@@ -3,24 +3,15 @@ import { expect } from '@playwright/test';
 import { PlaywrightWorld } from '@gnuechtel/nx-cucumber';
 import { maskColor } from '../shared';
 
-When(
-  'the user requests project information',
-  async function (this: PlaywrightWorld) {
-    const whatsNextButton = this.page.getByRole('link', {
-      name: "What's next?",
-    });
-    this.setScreenshotOptions({ mask: [whatsNextButton], maskColor }); // highlight what-is-next link
-    await whatsNextButton.click();
-  }
-);
-
 Then(
   /^the user is shown a card with information about repair requests$/,
   async function (this: PlaywrightWorld) {
     const repairRequestsCard = this.page.locator(
       '.MuiCard-root >> text="Repair Requests"'
     );
+
     this.setScreenshotOptions({ mask: [repairRequestsCard], maskColor }); // highlight next-steps headline
+
     await repairRequestsCard.scrollIntoViewIfNeeded();
     await expect(repairRequestsCard).toBeVisible();
   }
@@ -32,7 +23,9 @@ Then(
     const welcome = this.page
       .locator('h1')
       .getByText('Welcome to the Apartment Complex Utility App');
+
     this.setScreenshotOptions({ mask: [welcome], maskColor }); // highlight welcome headline
+
     await expect(welcome).toBeVisible();
   }
 );
