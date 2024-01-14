e4bf0b586ed2212e3cf6f84d21893db93bc88375
Max <hello@mpfglaser.nl>
2024-01-14 22:41:50 +0100

    refactor(sequelize): add retry

--- apps/location-service/src/main.ts
+++ apps/location-service/src/main.ts
@@ -2,15 +2,22 @@ import sequelize from './config/sequelize.init';
 import app from './app';
 
 (async () => {
-  // Try to connect to the database and sync the models
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    await sequelize.sync();
-    console.log('All models were synchronized successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-    process.exit(1);
+  for (let i = 0; i < 5; i++) {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+      await sequelize.sync();
+      console.log('All models were synchronized successfully.');
+      break; // Exit the loop once the connection is successful
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      if (i < 4) {
+        // Don't wait after the last attempt
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds before trying again
+      } else {
+        process.exit(1);
+      }
+    }
   }
 
   const port = process.env.PORT || 3000;
