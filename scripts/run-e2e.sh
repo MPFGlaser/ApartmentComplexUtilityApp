37b636d8749d6e1efd4ba109ef308644521aed25
Max <hello@mpfglaser.nl>
2024-01-15 00:31:20 +0100

    fix(gha/e2e): final attempt tonight

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start all microservices
-npx nx run-many --target=serve --all --exclude=acua &
+nohup npx nx run-many --target=serve --all --exclude=acua &
 
 # Wait for all microservices to start
 npx wait-on http://localhost:3100/health && printf "location-service is healthy\n"
@@ -10,3 +10,9 @@ npx wait-on http://localhost:3300/health && printf "user-service is healthy\n"
 
 # Run e2e tests
 npx nx e2e acua-e2e
+
+# Stop all microservices
+pkill -f "run-many --target=serve --all --exclude=acua" || true
+
+# Wait until all instances of the command are killed
+while pgrep -f "npx nx run-many --target=serve --all --exclude=acua" > /dev/null; do sleep 1; done
